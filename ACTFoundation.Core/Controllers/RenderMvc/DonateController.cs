17c101bba0c7e010fa2e03dda54851deaeaaf05a
galeksic <gavrilo.aleksic93@gmail.com>
2021-09-25 17:26:55 +0200

    Created donate controller

---
+++ ACTFoundation.Core/Controllers/RenderMvc/DonateController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using ACTFoundation.Core.ViewModels.Pages;
+using ACTFoundation.Models.Generated;
+
+namespace ACTFoundation.Core.Controllers.RenderMvc
+{
+	public class DonateController : BasePageController<Donate>
+	{
+		public ActionResult Index(Donate model)
+			=> CurrentTemplate(new DonateViewModel(CreatePageContext(model)));
+	}
+}
---
+++ ACTFoundation.Core/ViewModels/Pages/DonateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ACTFoundation.Core.Contexts;
+using ACTFoundation.Core.ViewModels.Blocks;
+using ACTFoundation.Core.ViewModels.Partials.Testimonials;
+using ACTFoundation.Models.Generated;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace ACTFoundation.Core.ViewModels.Pages
+{
+	public class DonateViewModel : PageViewModel
+	{
+		public DonateViewModel(IPageContext<Donate> context) : base(context)
+		{
+			
+		}
+	}
+}
