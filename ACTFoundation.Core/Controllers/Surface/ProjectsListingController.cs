8fc0460ff42f1b8712fbda7b78bf11380034d955
Dejan Besic <dejanb@unitestudents.com>
2021-09-26 00:35:54 +0200

    Projects pagination

---
+++ ACTFoundation.Core/Controllers/Surface/ProjectsListingController.cs
@@ -0,0 +1,24 @@
+﻿using ACTFoundation.Core.Models;
+using ACTFoundation.Core.ViewModels.Partials.Blocks;
+using ACTFoundation.Models.Generated;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace ACTFoundation.Core.Controllers.Surface.Partials
+{
+    public class ProjectsListingController : SurfaceController 
+    {
+		public ActionResult GetMoreProjects(Guid projectsId, int pageNumber = 1)
+		{
+			var projectsPage = Umbraco.Content(projectsId) as Projects;
+			var articles = projectsPage.DescendantsOfType(Project.ModelTypeAlias)
+				.Select(x => new ProjectCardViewModel(x as Project))
+				.ToList();
+
+			return PartialView(new LoadmorePagination<ProjectCardViewModel>(projectsId, pageNumber, 6, articles));
+		}
+	}
+}
