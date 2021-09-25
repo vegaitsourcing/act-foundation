f489e4189cc3a1aade711d5a205f867ec5cb9d31
Bojan Čoka <coka@fastmail.com>
2021-09-25 10:43:31 +0200

    Link CSS in Pug

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,4 +3,10 @@ const pug = require("gulp-pug");
 
 const { src, dest } = gulp;
 
-gulp.task("build", () => src("./src/*.pug").pipe(pug()).pipe(dest("./dist")));
+gulp.task("build", (done) => {
+  gulp.parallel("pug", "css")(done);
+});
+
+gulp.task("pug", () => src("./src/*.pug").pipe(pug()).pipe(dest("./dist")));
+
+gulp.task("css", () => src("./index.css").pipe(dest("./dist")));
