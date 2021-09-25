359027c88800304cc85b82f2da1574ee120a11b8
Srdjan Ivkovic <s.ivkovic@vegait.rs>
2021-09-25 12:51:47 +0200

    CurrentProjectsViewModel + ProjectViewModel

---
+++ ACTFoundation.Core/ViewModels/Partials/Layout/ProjectViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ACTFoundation.Core.ViewModels.Shared;
+using ACTFoundation.Models.Generated;
+using System;
+using Umbraco.Core;
+using Umbraco.Web;
+
+namespace ACTFoundation.Core.ViewModels.Partials.Layout
+{
+    public class ProjectViewModel
+    {
+        private const int ShortTextLength = 208;
+
+        public ProjectViewModel(Project project)
+        {
+            if (project == null) throw new ArgumentNullException(nameof(project));
+
+            this.Title = project.Title;
+            this.ShortText = project.Text.ToHtmlString().StripHtml().Substring(0, ShortTextLength);
+            this.MainImage = new ImageViewModel(project.MainImage);
+            this.Url = project.Url();
+        }
+
+        public string Title { get; set; }
+        public string ShortText { get; set; }
+        public ImageViewModel MainImage { get; set; }
+        public string Url { get; set; }
+    }
+}
