6a25c6b30822b021278e3906a8bb77c43cc875e7
VEGA\s.brzak <sonja.brzak@cornelsen.de>
2021-09-25 12:07:21 +0200

    Implemented TopBottomSocialLinks ViewModel and View. Added style.css

---
+++ ACTFoundation.Core/ViewModels/Partials/Layout/TopBottomSocialLinksViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ACTFoundation.Core.ViewModels.Shared;
+using ACTFoundation.Models.Generated;
+
+namespace ACTFoundation.Core.ViewModels.Partials.Layout
+{
+    public class TopBottomSocialLinksViewModel
+    {
+        public TopBottomSocialLinksViewModel(ITopBottomSocialLinks topBottomSocialLinks)
+        {
+            DonateLink = new LinkViewModel(topBottomSocialLinks.DonateLink);
+            FacebookLink = topBottomSocialLinks.FacebookLink;
+            LinkedinLink = topBottomSocialLinks.LinkedinLink;
+            InstagramLink = topBottomSocialLinks.InstagramLink;
+        }
+
+        public LinkViewModel DonateLink { get; }
+
+        public string FacebookLink { get; }
+
+        public string LinkedinLink { get; }
+
+        public string InstagramLink { get; }
+    }
+}
