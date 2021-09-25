cb3b0bc904668983a4edce16e24234dde7686297
m-gula <m.gula@vegaitsourcing.rs>
2021-09-25 22:22:20 +0200

    Leftover of loadmore news implementation

---
+++ ACTFoundation.Core/Controllers/Surface/NewsController.cs
@@ -0,0 +1,24 @@
+﻿using ACTFoundation.Core.Models;
+using ACTFoundation.Core.ViewModels.Blocks;
+using ACTFoundation.Models.Generated;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace ACTFoundation.Core.Controllers.Surface
+{
+	public class NewsController : SurfaceController
+	{
+		public ActionResult GetMoreNews(Guid blogId, int pageNumber = 1)
+		{
+			var blogPage = Umbraco.Content(blogId) as Blog;
+			var articles = blogPage.DescendantsOfType(Article.ModelTypeAlias)
+				.Select(x => new ArticleCardViewModel(x as Article))
+				.ToList();
+
+			return PartialView(new LoadmorePagination<ArticleCardViewModel>(blogId, pageNumber, 6, articles));
+		}
+	}
+}
---
+++ ACTFoundation.Core/ViewModels/Blocks/NewsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ACTFoundation.Models.Generated;
+using System;
+using System.Linq;
+
+namespace ACTFoundation.Core.ViewModels.Blocks
+{
+    public class NewsViewModel
+    {
+        public NewsViewModel(News news)
+        {
+            BlogId = news.Blog.FirstOrDefault()?.Key;
+        }
+
+        public Guid? BlogId { get; }
+    }
+}
