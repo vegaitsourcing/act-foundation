d6b301a91a76c8677a7c341334648144b209099c
Vuceticj <j.vucetic@vegaitsourcing.rs>
2021-09-25 14:57:49 +0200

    Added testimonials feature

--- ACTFoundation.Core/ViewModels/Shared/ImageViewModel.cs
+++ ACTFoundation.Core/ViewModels/Shared/ImageViewModel.cs
@@ -7,7 +7,7 @@ namespace ACTFoundation.Core.ViewModels.Shared
 	public class ImageViewModel
 	{
 		public ImageViewModel(string url, string urlAbsolute, string type, 
-			int width, int height, string alternateText = null)
+			int? width, int? height, string alternateText = null)
 		{
 			Url = url;
 			UrlAbsolute = urlAbsolute;
@@ -18,15 +18,15 @@ namespace ACTFoundation.Core.ViewModels.Shared
 		}
 
 		public ImageViewModel(Image image) 
-			: this(image.Url(), image.MediaUrl(mode: UrlMode.Absolute), image.Type,
-				  image.Width, image.Height, string.IsNullOrWhiteSpace(image.AlternateText) ? image.Name : image.AlternateText)
+			: this(image?.Url(), image?.MediaUrl(mode: UrlMode.Absolute), image?.Type,
+				  image?.Width, image?.Height, string.IsNullOrWhiteSpace(image?.AlternateText) ? image?.Name : image?.AlternateText)
 		{ }
 
 		public string Url { get; }
 		public string UrlAbsolute { get; }
 		public string Type { get; }
-		public int Width { get; }
-		public int Height { get; }
+		public int? Width { get; }
+		public int? Height { get; }
 		public string AlternateText { get; }
 	}
 }
