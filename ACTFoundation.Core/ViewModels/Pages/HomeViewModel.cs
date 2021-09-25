b9d4de25881ce37ed81e22a633adbe5227296bd8
Vuceticj <j.vucetic@vegaitsourcing.rs>
2021-09-25 16:05:45 +0200

    Changed generated to viewModels for banner carousel, banner default and link button

--- ACTFoundation.Core/ViewModels/Pages/HomeViewModel.cs
+++ ACTFoundation.Core/ViewModels/Pages/HomeViewModel.cs
@@ -1,9 +1,8 @@
 ﻿using ACTFoundation.Core.Contexts;
 using ACTFoundation.Core.ViewModels.Partials.Testimonials;
+using ACTFoundation.Core.ViewModels.Shared;
 using ACTFoundation.Models.Generated;
-using System.Collections.Generic;
 using System.Linq;
-using Umbraco.Core.Models.PublishedContent;
 
 namespace ACTFoundation.Core.ViewModels.Pages
 {
@@ -11,16 +10,20 @@ namespace ACTFoundation.Core.ViewModels.Pages
 	{
 		public HomeViewModel(IPageContext<Home> context) : base(context)
 		{
-			MainContent = context.Page.MainContent.Where(item => !(item is Testimonials));
-			
+			var bannerCarousel = context.Page.MainContent.FirstOrDefault(item => item is BannerCarousel);
+            if(bannerCarousel != null)
+			{
+				BannerCarousel = new BannerCarouselViewModel(bannerCarousel as BannerCarousel);
+            }
+
 			var testimonials = context.Page.MainContent.FirstOrDefault(item => item is Testimonials);
 			if(testimonials != null)
             {
-				Testimonials = new TestimonialsViewModel((Testimonials)testimonials);
+				Testimonials = new TestimonialsViewModel(testimonials as Testimonials);
             }
 		}
 
-        public IEnumerable<IPublishedElement> MainContent { get; }
+		public BannerCarouselViewModel BannerCarousel { get; set; }
 
 		public TestimonialsViewModel Testimonials { get; }
     }
