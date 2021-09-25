9aab713b6df5d104b25dd235de70cf49cf4027df
Igor Gajic <igajic80@gmail.com>
2021-09-25 09:33:27 -0700

    Implemented contact page

---
+++ ACTFoundation.Core/ViewModels/Pages/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ACTFoundation.Core.Contexts;
+using ACTFoundation.Models.Generated;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace ACTFoundation.Core.ViewModels.Pages
+{
+    public class ContactViewModel : PageViewModel
+    {
+        public ContactViewModel(IPageContext<Contact> context) : base(context)
+        {
+            MainContent = context.Page.ContactContent;
+            IsSubmitted = false;
+        }
+
+        public IPublishedElement MainContent { get; set; }
+
+        public bool IsSubmitted { get; set; }
+    }
+}
