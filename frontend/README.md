088a46cb8cad99fcb8bd943eb2ee7e8580792204
Bojan Čoka <coka@fastmail.com>
2021-09-25 11:00:06 +0200

    Concatenate and minify CSS

--- frontend/README.md
+++ frontend/README.md
@@ -2,6 +2,9 @@
 
 https://vegaitsourcing.github.io/act-foundation/
 
+**Minified CSS:**
+https://raw.githubusercontent.com/vegaitsourcing/act-foundation/gh-pages/index.css
+
 ```
 npm install
 npm run build
--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
 
 const { src, dest } = gulp;
 
@@ -9,4 +11,9 @@ gulp.task("build", (done) => {
 
 gulp.task("pug", () => src("./src/*.pug").pipe(pug()).pipe(dest("./dist")));
 
-gulp.task("css", () => src("./index.css").pipe(dest("./dist")));
+gulp.task("css", () =>
+  src("./**/*.css")
+    .pipe(cleanCSS())
+    .pipe(concat("index.css"))
+    .pipe(dest("./dist"))
+);
