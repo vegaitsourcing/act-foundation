ae065d7559d016c6d8cb9bd2a0d1624672f3ce64
Igor Gajic <i.gajic@vegait.rs>
2021-09-26 01:44:47 -0700

    Added newsletter subscription table and endpoints

---
+++ ACTFoundation.Core/Composers/NewsletterSubscriptionComposer.cs
@@ -0,0 +1,14 @@
+﻿using ACTFoundation.Core.Components;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace ACTFoundation.Core.Composers
+{
+    public class NewsletterSubscriptionComposer: IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<NewsletterSubscriptionComponent>();
+        }
+    }
+}
