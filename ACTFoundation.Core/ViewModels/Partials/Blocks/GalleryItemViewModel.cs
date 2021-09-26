063592e56e6cfe3726fde8114ce332fe2abe95b0
Igor Gajic <i.gajic@vegait.rs>
2021-09-26 02:19:12 -0700

    Added gallery block

---
+++ ACTFoundation.Core/ViewModels/Partials/Blocks/GalleryItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ACTFoundation.Core.ViewModels.Shared;
+using ACTFoundation.Models.Generated;
+using System;
+using System.Globalization;
+
+namespace ACTFoundation.Core.ViewModels.Partials.Blocks
+{
+    public class GalleryItemViewModel
+    {
+        public GalleryItemViewModel(GalleryItem galleryItem)
+        {
+            GalleryImage = new ImageViewModel(galleryItem.GalleryImage);
+            GalleryDescription = galleryItem.GalleryDescription;
+            GalleryDate = galleryItem.GalleryDate.ToString("dd. MMMM yyyy", new CultureInfo("sr-Latn-CS"));
+        }
+
+        public ImageViewModel GalleryImage { get; set; }
+
+        public string GalleryDescription { get; set; }
+
+        public String GalleryDate { get; set; }
+    }
+}
---
+++ ACTFoundation.Core/ViewModels/Partials/Blocks/GalleryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ACTFoundation.Models.Generated;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ACTFoundation.Core.ViewModels.Partials.Blocks
+{
+    public class GalleryViewModel
+    {
+        public GalleryViewModel(Gallery gallery)
+        {
+            Title = gallery.Title;
+            Subtitle = gallery.Subtitle;
+            GalleryItems = gallery.GalleryItems.Select(galleryItem => new GalleryItemViewModel(galleryItem)).ToList();
+        }
+
+        public string Title { get; set; }
+
+        public string Subtitle { get; set; }
+
+        public List<GalleryItemViewModel> GalleryItems { get; set; }
+    }
+}
